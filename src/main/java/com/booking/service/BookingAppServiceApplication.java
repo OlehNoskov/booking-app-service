b7f83faaff92ee62baa9c743fd905f15272ff52a
Noskov, Oleh [PS] <oleh.noskov@cengage.com>
2024-09-06 12:39:02 +0300

    Init commit

---
+++ src/main/java/com/booking/service/BookingAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.booking.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppServiceApplication.class, args);
+    }
+
+}
